fd7c1df5a74d1d34087ae817eda923d323c8fd7f
ytt521 <3047893225@qq.com>
2022-11-21 01:07:16 +0800

    添加了一些依赖

---
+++ src/main/java/work/xiaoying/config/MybatisPlusConfig.java
@@ -0,0 +1,25 @@
+package work.xiaoying.config;
+
+import com.baomidou.mybatisplus.annotation.DbType;
+import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
+import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * mybatis +配置
+ *
+ * @author 挚爱小v脸
+ * @date 2022/11/21
+ * @since 1.8
+ */
+@Configuration
+public class MybatisPlusConfig {
+
+    @Bean
+    public MybatisPlusInterceptor mybatisPlusInterceptor() {
+        MybatisPlusInterceptor interceptor = new MybatisPlusInterceptor();
+        interceptor.addInnerInterceptor(new PaginationInnerInterceptor(DbType.MYSQL));
+        return interceptor;
+    }
+}
